a51a298144ff65f786c783e22d5028bf9223f69e
LucasDibz <lucas.f.dib@gmail.com>
2023-09-08 18:10:26 +0100

    feat: copy https://github.com/web3templates/nextly-template/blob/main/tailwind.config.js landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,56 @@
-import { useFeatureFlags } from './hooks/useFeatureFlags';
+import Hero from './components/hero';
+import Navbar from './components/navbar';
+import SectionTitle from './components/sectionTitle';
 
-function App() {
-  const {
-    features: { isFeature1Enabled, isFeature2Enabled },
-  } = useFeatureFlags();
+import { benefitOne, benefitTwo } from './components/data';
+import Video from './components/video';
+import Benefits from './components/benefits';
+import Footer from './components/footer';
+import Testimonials from './components/testimonials';
+import Cta from './components/cta';
+import Faq from './components/faq';
 
+const Home = () => {
   return (
-    <div className='bg-gray-500 w-screen h-screen text-white'>
-      <header className='text-center'>Header</header>
-      <main className='flex flex-col items-center gap-5 border-red-100 rounded'>
-        {isFeature1Enabled && <div>FEATURE 1</div>}
-
-        {isFeature2Enabled && <div>FEATURE 2</div>}
-      </main>
-    </div>
+    <>
+      <Navbar />
+      <Hero />
+      <SectionTitle
+        pretitle='Nextly Benefits'
+        title=' Why should you use this landing page'
+      >
+        Nextly is a free landing page & marketing website template for startups
+        and indie projects. Its built with Next.js & TailwindCSS. And its
+        completely open-source.
+      </SectionTitle>
+      <Benefits data={benefitOne} />
+      <Benefits imgPos='right' data={benefitTwo} />
+      <SectionTitle
+        pretitle='Watch a video'
+        title='Learn how to fullfil your needs'
+      >
+        This section is to highlight a promo or demo video of your product.
+        Analysts says a landing page with video has 3% more conversion rate. So,
+        don&apos;t forget to add one. Just like this.
+      </SectionTitle>
+      <Video />
+      <SectionTitle
+        pretitle='Testimonials'
+        title="Here's what our customers said"
+      >
+        Testimonails is a great way to increase the brand trust and awareness.
+        Use this section to highlight your popular customers.
+      </SectionTitle>
+      <Testimonials />
+      <SectionTitle pretitle='FAQ' title='Frequently Asked Questions'>
+        Answer your customers possible questions here, it will increase the
+        conversion rate as well as support or chat requests.
+      </SectionTitle>
+      <Faq />
+      <Cta />
+      <Footer />
+    </>
   );
-}
+};
 
-export default App;
+export default Home;
